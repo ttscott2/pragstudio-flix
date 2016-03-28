e3912c6959e2c2beb0ab396a01abf4dab5632703
ttscott2 <timothytscott3@gmail.com>
2016-03-27 19:44:56 -0500

    add the new page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,8 +14,23 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params.require(:movie).permit(:title, :rating, :description, :total_gross, :released_on)
     @movie.update(movie_params)
     redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :total_gross, :released_on)
   end
 end
